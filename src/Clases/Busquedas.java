7253f5e820419ed01590494bb847c7986ab29d05
Karenop4 <karenorpe2004@gmail.com>
2024-12-23 18:57:13 -0500

    Initial commit

---
+++ src/Clases/Busquedas.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author USER
+ */
+public class Busquedas {
+    //Metodo para validar el usuario
+    public boolean validarUsr(String usr, String cont){
+        //CODIGO PARA VALIDAR EL USUARIO EN LA BASE DE DATOS
+        
+        return true;
+    }
+    //Metodo para validar existencia de un cliente 
+    public boolean validarCliente(String ced){
+        return true;
+    }
+}
