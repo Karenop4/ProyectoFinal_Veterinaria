8feb9bb2d2c11044d231ea41cd56777a0db7c21e
Andrés Encalada <151053871+AndresEncalada@users.noreply.github.com>
2025-02-06 22:05:58 -0500

    Actualización SQL
    Inserción de datos

--- Final_ER_Correccion/final_vet.sql
+++ Final_ER_Correccion/final_vet.sql
@@ -1,3 +1,16 @@
+----TABLESPACE y CREACION DE USUARIO-------
+/*
+alter session set "_ORACLE_SCRIPT"=TRUE;
+create user admin_vet identified by 1234;
+*/
+create tablespace TB_VET_FINAL datafile 'tbs_vet_final.ora' size 40M online;
+create temporary tablespace TBS_VET_FINAL_TEMP tempfile 'tbs_vet_final_temp.ora' size 5M autoextend on;
+
+alter user admin_vet default tablespace TBS_VET_FINAL
+temporary tablespace TBS_VET_FINAL_TEMP
+quota unlimited on TBS_VET_FINAL;
+
+
 CREATE TABLE vet_citas (
     cita_id              NUMBER(5) NOT NULL,
     cita_fecha           TIMESTAMP(0) NOT NULL,
@@ -222,14 +235,12 @@ ALTER TABLE vet_citas
     ADD CONSTRAINT vet_citas_vet_personas_fk FOREIGN KEY ( vet_personas_per_id )
         REFERENCES vet_personas ( per_id );
 
---  ERROR: FK name length exceeds maximum allowed length(30) 
 ALTER TABLE vet_detalles_fac
-    ADD CONSTRAINT vet_detalles_fac_vet_facturas_fk FOREIGN KEY ( vet_facturas_fac_id )
+    ADD CONSTRAINT vet_detf_fac_fk FOREIGN KEY ( vet_facturas_fac_id )
         REFERENCES vet_facturas ( fac_id );
-
---  ERROR: FK name length exceeds maximum allowed length(30) 
+ 
 ALTER TABLE vet_detalles_fac
-    ADD CONSTRAINT vet_detalles_fac_vet_servicios_fk FOREIGN KEY ( vet_servicios_serv_codigo )
+    ADD CONSTRAINT vet_detf_serv_fk FOREIGN KEY ( vet_servicios_serv_codigo )
         REFERENCES vet_servicios ( serv_codigo );
 
 ALTER TABLE vet_facturas
@@ -243,10 +254,9 @@ ALTER TABLE vet_facturas
 ALTER TABLE vet_mascotas
     ADD CONSTRAINT vet_mascotas_vet_personas_fk FOREIGN KEY ( vet_personas_per_id )
         REFERENCES vet_personas ( per_id );
-
---  ERROR: FK name length exceeds maximum allowed length(30) 
+ 
 ALTER TABLE vet_mascotas
-    ADD CONSTRAINT vet_mascotas_vet_tiposmascotas_fk FOREIGN KEY ( vet_tiposmascotas_tipom_id )
+    ADD CONSTRAINT vet_masc_tp_fk FOREIGN KEY ( vet_tiposmascotas_tipom_id )
         REFERENCES vet_tiposmascotas ( tipom_id );
 
 ALTER TABLE vet_razas
@@ -257,7 +267,77 @@ ALTER TABLE vet_usuarios
     ADD CONSTRAINT vet_usuarios_vet_personas_fk FOREIGN KEY ( vet_personas_per_id )
         REFERENCES vet_personas ( per_id );
         
-        
-INSERT INTO vet_personas VALUES (1,'0150741742', 'Karen', 'Ortiz', 'El Valle', '0994441682', 'karenorpe2004@gmail.com', 'C', '', 'A');
+-----------------------------------------------
+-- Secuencias para auto-increment de tablas ---
+create sequence v_tmas_seq start with 1;
+create sequence v_ra_seq start with 1;
+create sequence v_masc_seq start with 1;
+create sequence v_per_seq start with 1;
+create sequence v_cit_seq start with 1;
+create sequence v_us_seq start with 1;
+create sequence v_fac_seq start with 1;
+create sequence v_serv_seq start with 1;
+create sequence v_detf_seq start with 1;
+------------------------------------------------
+------------------------------------------------
+----------------------------    TIPOS DE MASCOTAS   ----------------------------
+insert into vet_tiposmascotas values (v_tmas_seq.nextval,'canino');
+insert into vet_tiposmascotas values (v_tmas_seq.nextval,'felino');
+insert into vet_tiposmascotas values (v_tmas_seq.nextval,'reptil');
+insert into vet_tiposmascotas values (v_tmas_seq.nextval,'roedor');
+insert into vet_tiposmascotas values (v_tmas_seq.nextval,'otro');
+--select * from vet_tiposmascotas;
+--------------------------------------------------------------------------------
+----------------------------    RAZAS   ----------------------------------------
+insert into vet_razas values(v_ra_seq.nextval,'Siames',(select tipom_id from vet_tiposmascotas where tipom_nombre like 'felino'));
+insert into vet_razas values(v_ra_seq.nextval,'Labrador',(select tipom_id from vet_tiposmascotas where tipom_nombre like 'canino'));
+insert into vet_razas values(v_ra_seq.nextval,'n/d',(select tipom_id from vet_tiposmascotas where tipom_nombre like 'otro'));
+--SELECT * FROM vet_razas;
+--------------------------------------------------------------------------------
+----------------------------    MASCOTAS   -------------------------------------
+insert into vet_mascotas values(v_masc_seq.nextval,'Rufus',(select per_id from vet_personas where per_nombre like 'Andrés'),(
+select tipom_id from vet_tiposmascotas where tipom_nombre like 'canino'));
+insert into vet_mascotas values(v_masc_seq.nextval,'Toby',(select per_id from vet_personas where per_nombre like 'Karen'),(
+select tipom_id from vet_tiposmascotas where tipom_nombre like 'canino'));
+--select * from vet_mascotas;
+--------------------------------------------------------------------------------
+----------------------------    PERSONAS   -------------------------------------
+INSERT INTO vet_personas VALUES (v_per_seq.nextval,'0000000000', 'Admin', 'Vet', '-', '0000000000', 'admin@ejemplo.com', 'E', 'A', 'A');
+INSERT INTO vet_personas VALUES (v_per_seq.nextval,'0950094508', 'Andrés', 'Encalada', '-', '0983586619', 'andres23102004@gmail.com', 'C', null, 'A');
+INSERT INTO vet_personas VALUES (v_per_seq.nextval,'0000000000', 'Karen', 'Ortiz', '-', '0000000000', 'karen@ejemplo.com', 'C', null, 'A');
+INSERT INTO vet_personas VALUES (v_per_seq.nextval,'0124466754', 'Carlos', 'General', '-', '0937162846', 'carlos@ejemplo.com', 'E', 'V', 'A');
+INSERT INTO vet_personas VALUES (v_per_seq.nextval,'1123544634', 'Mateo', 'Suarez', '-', '0987654321', 'mateo@ejemplo.com', 'E', 'A', 'A');
+INSERT INTO vet_personas VALUES (v_per_seq.nextval,'0124435456', 'Paula', 'Acevedo', '-', '0981253462', 'paula@ejemplo.com', 'E', 'A', 'I');
+--select * from vet_personas;
+--------------------------------------------------------------------------------
+----------------------------    CITAS   ----------------------------------------
+insert into vet_citas values(v_cit_seq.nextval,(TO_TIMESTAMP('2025/02/15 10:30:00', 'YYYY/MM/DD HH24:MI:SS')),'A',2,
+(select vet_personas_per_id from vet_mascotas where masc_id like 2));
+--select * from vet_citas;
+--------------------------------------------------------------------------------
+----------------------------    USUARIOS   -------------------------------------
+insert into vet_usuarios values (v_us_seq.nextval,'admin','admin','A',(select per_id from vet_personas where per_nombre like 'Admin'));
+insert into vet_usuarios values (v_us_seq.nextval,'usr','usr','G',(select per_id from vet_personas where per_nombre like 'Carlos'));
+insert into vet_usuarios values (v_us_seq.nextval,'mat123','123','G',(select per_id from vet_personas where per_nombre like 'Mateo'));
+insert into vet_usuarios values (v_us_seq.nextval,'pau123','123','G',(select per_id from vet_personas where per_nombre like 'Paula'));
+--select *from vet_usuarios;
+--------------------------------------------------------------------------------
+----------------------------    FACTURAS   -------------------------------------
+insert into vet_facturas values (v_fac_seq.nextval,(TO_DATE('2024/02/20 10:35:23', 'yyyy/mm/dd hh24:mi:ss')),52.17,7.83,60,
+(select us_id from vet_usuarios where us_usuario like 'mat123'),(select per_id from vet_personas where per_nombre like 'Andrés'));
+--select * from vet_facturas;
+--------------------------------------------------------------------------------
+----------------------------    SERVICIOS  -------------------------------------
+insert into vet_servicios values (v_serv_seq.nextval, 'Estirilización Perro Raza Pequeña',120,'A','S');
+insert into vet_servicios values (v_serv_seq.nextval, 'Estirilización Perro Raza Mediana',150,'A','S');
+insert into vet_servicios values (v_serv_seq.nextval, 'Estirilización Perro Raza Grande',200,'I','S');
+insert into vet_servicios values (v_serv_seq.nextval, 'Estirilización Gato Pequeño',80,'A','S');
+insert into vet_servicios values (v_serv_seq.nextval, 'Vacunacion completa',30.43,'A','N');
+--select * from vet_servicios;
+--------------------------------------------------------------------------------
+----------------------------    DETALLES FACTURA   -----------------------------
+insert into vet_detalles_fac values (v_detf_seq.nextval,1,30.43,0,30.43,30.43,(select serv_codigo from vet_servicios where serv_nombre like 'Vacunacion completa'),
+(select fac_id from vet_facturas where fac_id like 2 ));
+
+--select * from vet_detalles_fac;
 commit;
-SELECT * FROM vet_personas;
